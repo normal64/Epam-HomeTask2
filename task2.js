1a0839a077c05565f71b29511a95d0214aaad47c
Denis <normal64@mail.ru>
2023-08-23 10:40:24 +0400

    fix

--- task2.js
+++ task2.js
@@ -2,36 +2,34 @@ function parseAndStore(str) {
   const data = {};
 
   str.split(", ").forEach((el) => {
-      const [key, value] = el.split(" - ");
-      data[key] = value;
-  })
+    const [key, value] = el.split(" - ");
+    data[key] = value;
+  });
 
   //console.log("data", data);
-  let dataFromStorage = localStorage.getItem("data")
+  let dataFromStorage = localStorage.getItem("data");
   console.log(dataFromStorage);
   let existingDataObj;
   if (dataFromStorage) {
-      existingDataObj = JSON.parse(dataFromStorage);
-      const existingKeys = Object.keys(existingDataObj)
-      const newKeys = Object.keys(data)
-      const keysMatch = existingKeys.every(key => newKeys.includes(key)
-      )
-      if (keysMatch) {
-          existingDataObj = { ...existingDataObj, ...data }
-          console.log("key match object", existingDataObj);
-          localStorage.setItem('data', JSON.stringify(existingDataObj));
-      } else {
-          console.log("keys do not match")
-          const timestamp = new Date().getTime();
-          existingDataObj[`data_${timestamp}`] = data;
-          localStorage.setItem('data', JSON.stringify(existingDataObj));
-      }
+    existingDataObj = JSON.parse(dataFromStorage);
+    const existingKeys = Object.keys(existingDataObj);
+    const newKeys = Object.keys(data);
+    const keysMatch = existingKeys.every((key) => newKeys.includes(key));
+    if (keysMatch) {
+      existingDataObj = { ...existingDataObj, ...data };
+      console.log("key match object", existingDataObj);
+      localStorage.setItem("data", JSON.stringify(existingDataObj));
+    } else {
+      console.log("keys do not match");
+      const timestamp = new Date().getTime();
+      existingDataObj[`data_${timestamp}`] = data;
+      localStorage.setItem("data", JSON.stringify(existingDataObj));
+    }
   } else {
-      existingDataObj = data;
+    existingDataObj = data;
 
-      localStorage.setItem('data', JSON.stringify(existingDataObj));
+    localStorage.setItem("data", JSON.stringify(existingDataObj));
   }
-
 }
 parseAndStore("city - London, country - United Kingdom");
 
